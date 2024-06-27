da9d6759c1a1992c4fc297084c5d7f21342a05e2
dharamveergit <dharamveersingh.em@gmail.com>
2024-06-27 21:29:27 +0530

    refactor: Update Chat page layout and add interactive cards

--- src/app/(home)/chat/page.tsx
+++ src/app/(home)/chat/page.tsx
@@ -1,9 +1,56 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout";
+import { ArrowUp, Code, Pen, Zap } from "lucide-react";
+
+const cards = [
+  {
+    title: "“Which proposals are currently gaining a lot of support”",
+    icon: Pen
+  },
+  {
+    title:
+      "“Tell me about the most important things that happened in the ecosystem in the past 24 hours”",
+    icon: Zap
+  },
+  {
+    title: "“What's going on in the discord and telegram channels”",
+    icon: Code
+  }
+];
 
 const Chat = () => {
   return (
-    <ContentLayout title="Chat" className="space-y-10">
-      Chat
+    <ContentLayout
+      title="Chat"
+      newClass=" flex flex-col h-[90dvh] relative gap-6"
+    >
+      <div className="flex-1 overflow-y-auto flex items-center justify-center container">
+        <div className="flex items-center justify-center  w-[70%] flex-col gap-10  ">
+          <img src="/eye.svg" alt="Chat" className="w-10" />
+          <div className="grid grid-cols-3 gap-5">
+            {cards.map((card, index) => (
+              <div
+                key={index}
+                className="flex flex-col rounded-lg text-muted-foreground p-4  gap-5 border bg-background "
+              >
+                <card.icon size={20} />
+                <p className="text-sm">{card.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="border p-2 bg-background rounded-full  w-[70%] flex items-center mx-auto mb-6">
+          <input
+            type="text"
+            placeholder="Ask DAOi "
+            className="w-full bg-transparent flex-1 focus:outline-none px-8  placeholder:text-muted-foreground"
+          />
+          <button className="flex items-center text-white justify-center bg-primary rounded-full h-full aspect-square p-1.5">
+            <ArrowUp />
+          </button>
+        </div>
+      </div>
     </ContentLayout>
   );
 };
--- src/components/admin-panel/content-layout.tsx
+++ src/components/admin-panel/content-layout.tsx
@@ -5,17 +5,25 @@ interface ContentLayoutProps {
   title: string;
   children: React.ReactNode;
   className?: string;
+  newClass?: string;
 }
 
 export function ContentLayout({
   title,
   children,
-  className
+  className,
+  newClass
 }: ContentLayoutProps) {
   return (
     <div>
       <Navbar title={title} />
-      <div className={cn("container pt-8 pb-8 px-4 sm:px-8", className)}>
+      <div
+        className={
+          newClass
+            ? newClass
+            : cn("container pt-8 pb-8 px-4 sm:px-8", className)
+        }
+      >
         {children}
       </div>
     </div>
