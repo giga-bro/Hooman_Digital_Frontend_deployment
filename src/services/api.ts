74031c0cfcd1a7d82f69b9e4d5d94a8fe4e0678b
giga-bro <aditya.sakpal2008@gmail.com>
2024-09-06 02:17:49 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { Latest } from "@/types/latest";
 import axios from "axios";
 
 // export const BASE_URL = "https://llmgovserver.onrender.com";
-export const BASE_URL = "http://127.0.0.1:8000"
+export const BASE_URL = "https://hooman-digital-llp-chat-server-1.onrender.com"
 
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
